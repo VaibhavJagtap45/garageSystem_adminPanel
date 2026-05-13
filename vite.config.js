3f940158c639c5c3e455ea611848be90818b6b7d
VaibhavJagtap45 <jrpant091@gmail.com>
2026-05-13 18:06:06 +0530

    admin  final panel

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  const backendUrl = env.VITE_API_BASE || "http://localhost:4000";
-
+  const backendUrl = env.VITE_API_BASE
   return {
     plugins: [react()],
     server: {
